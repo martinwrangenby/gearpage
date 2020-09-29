171946b40de2d0df41cd2a6a18b81b8f500cdf67
Martin Wrangenby <martinwrangenby@gmail.com>
2020-09-29 07:28:59 +0200

    Add dummy data

---
+++ src/assets/db.js
@@ -0,0 +1,55 @@
+const db = {
+  instruments: 
+  [
+    {
+      name: 'Hagström Jazzbass',
+      type: 'bass',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 1
+    },
+    {
+      name: 'Squier Bronco Bass',
+      type: 'bass',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 2
+    },
+    {
+      name: 'Gretsch Committee Bass',
+      type: 'bass',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 3
+    },
+    {
+      name: 'Noname Jazzbass',
+      type: 'bass',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 4
+    },
+    {
+      name: 'Noname Rickenbacker Copy',
+      type: 'bass',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 5
+    },
+    {
+      name: 'Squier Bass VI',
+      type: 'bass',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 6
+    },
+    {
+      name: 'Hagström Scandi',
+      type: 'guitar',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 7
+    },
+    {
+      name: 'Orange Rockerverb 100',
+      type: 'amp',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      id: 8
+    },
+  ]
+};
+
+export default db;
