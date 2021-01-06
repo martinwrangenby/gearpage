72fa5a5ce90a58461e87f0f565fa962355a4cd72
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-06 23:49:26 +0100

    Add app root routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import InstrumentList from './containers/InstrumentList/InstrumentList';
+import InstrumentDetails from './components/InstrumentDetails/InstrumentDetails';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 const App = () => {
   
   return (
-    <div className="App">
-      <InstrumentList/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path='/gearitem' component={InstrumentDetails}/>
+          <Route path='/' component={InstrumentList}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
