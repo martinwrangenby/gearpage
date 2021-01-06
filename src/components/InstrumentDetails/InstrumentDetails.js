ae7061b2c61fde9758407f416049d7b65bb6b714
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-06 21:09:25 +0100

    Add basic instrument details component

---
+++ src/components/InstrumentDetails/InstrumentDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const instrumentDetails = () => {
+  return (
+    <h1>What the F man?! this is the details page for an instrument</h1>
+  )
+}
+
+export default instrumentDetails;
