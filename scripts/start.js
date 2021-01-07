60c9ae084183c4ab7692a8d13d86c0ad3bb6863c
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-07 13:52:03 +0100

    Add check for DB URL specified as an env variable (dev only at this point)

--- scripts/start.js
+++ scripts/start.js
@@ -43,6 +43,15 @@ const isInteractive = process.stdout.isTTY;
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
+// TODO: the db URL is only read from the .env file and I haven't figured out how it should work for an deployed version
+// Therefore this check below is only user for npm start and not build at this point. DB in production needs to be handled later...
+// Warn and crash if db url isn't set
+if (!process.env.REACT_APP_DB) {
+  console.log(`
+    You need to specify your backend URL as the REACT_APP_DB env variable
+    More info on how to work with env variables: https://create-react-app.dev/docs/adding-custom-environment-variables/`);
+  process.exit();  
+}
 
 // Tools like Cloud9 rely on this.
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
