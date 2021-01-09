afd130eab8a79a97ec522309e10dd3182c198d54
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-10 00:33:29 +0100

    Add edit and delete support to InstrumentDetails component

--- src/containers/InstrumentDetails/InstrumentDetails.js
+++ src/containers/InstrumentDetails/InstrumentDetails.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Button from '../../components/UI/Button/Button';
-import axios from '../../axios';
+import Modal from '../../components/UI/Modal/Modal';
+import HandleInstrumentForm from '../../components/HandleInstrumentForm/HandleInstrumentForm';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import WithErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios';
 
 import './InstrumentDetails.css';
 
 const InstrumentDetails = (props) => {
   const [ instrument, setInstrument ] = React.useState(null);
+  const [ editingInstrument, setEditingInstrument ] = React.useState(false);
   const [ error, setError ] = React.useState(false);
   
   React.useEffect(() => { 
@@ -17,7 +20,7 @@ const InstrumentDetails = (props) => {
         axios.get(`/gear/${param[1]}.json`)
           .then(res => {
             if (res.data) {
-              setInstrument(res.data);
+              setInstrument({id: param[1], ...res.data});
             } else {throw new Error('Gear item couldn\'t be found in database')}
           })
           .catch(err => {
@@ -27,6 +30,28 @@ const InstrumentDetails = (props) => {
     }
 
   },[props.location.search])
+
+  const deleteInstrument = () => {
+    axios.delete(`/gear/${instrument.id}.json`)
+      .then(res => {
+        console.log(res);
+        props.history.replace('/');
+      })
+      .catch(err => {
+        setError(err);
+      })
+  }
+
+  const updateInstrument = (data) => {
+    axios.put(`/gear/${instrument.id}.json`, data)
+      .then(res => {
+        setInstrument({id: instrument.id, ...res.data});
+      })
+      .catch(err => {
+        setError(err);
+      })
+    setEditingInstrument(false);
+  }
 
   let content = <Spinner />
   if (error) {
@@ -42,9 +67,21 @@ const InstrumentDetails = (props) => {
   }
   return (
     <React.Fragment>
+      <Modal show={editingInstrument} modalClosed={() => setEditingInstrument(false)}>
+        <HandleInstrumentForm
+          instrument={instrument}
+          submitInstrument={updateInstrument}
+          closeModal={() => setEditingInstrument(false)}/>
+      </Modal>
       {content}
       <Button clicked={props.history.goBack}>
         Back
+      </Button>
+      <Button clicked={setEditingInstrument}>
+        Edit
+      </Button>
+      <Button buttonType='Danger' clicked={deleteInstrument}>
+        Delete
       </Button>
     </React.Fragment>
   )
