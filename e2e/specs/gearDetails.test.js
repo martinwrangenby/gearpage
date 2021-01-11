d2b22b82d142802f6093b65b49e35ded9d605721
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-11 08:10:59 +0100

    Add test for editing and deleting gear item

---
+++ e2e/specs/gearDetails.test.js
@@ -0,0 +1,28 @@
+const { addGearItem, deleteGearItem } = require('../utils/firebaseAPI');
+const { bass, guitar } = require('../assets/testdata');
+
+let id;
+
+beforeEach(async () => {
+  id = await addGearItem(bass);
+  await page.goto(`${process.env.REACT_APP_FRONTEND}gearitem?id=${id}`, {waitUntil: 'networkidle'});
+  await page.waitForLoadState('networkidle');
+});
+
+test('Delete instrument', async () => {
+  await page.click('[data-test-id="deleteInstrument"]');
+  await page.waitForLoadState('networkidle');
+  await expect(page).not.toHaveSelector(`#${id}`, {timeout: 1000})
+});
+
+test('Edit instrument', async () => {
+  await page.click('[data-test-id="editInstrument"]');
+  await page.selectOption('[data-test-id="formGearType"]', guitar.type);
+  await page.fill('[data-test-id="formGearName"]', guitar.name);
+  await page.fill('[data-test-id="formGearDescription"]', guitar.description);
+  await page.click('[data-test-id="submitGearFormButton"]');
+  await page.waitForSelector(`.${guitar.type}`)
+  await expect(page).toEqualText('[data-test-id="gearDetailsName"]', guitar.name)
+  await expect(page).toEqualText('[data-test-id="gearDetailsDescription"]', guitar.description)
+  await deleteGearItem(id);
+});
