16cb89ef6489877da1e758be3e74966fe53b7462
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-11 08:09:33 +0100

    Add firebase API support for the e2e tests

---
+++ e2e/utils/firebaseAPI.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_DB
+});
+
+const deleteGearItem = (id) => {
+  return instance.delete(`/gear/${id}.json`)
+    .then(res => null)
+    .catch(err => { throw new Error(`Could not delete id ${id} from DB`) })
+};
+
+const addGearItem = (gearItem) => {
+  return instance.post('/gear.json', gearItem)
+    .then(res => res.data.name)
+    .catch(err => { throw new Error(`Could not add gearItem: ${JSON.stringify(gearItem)}`) })
+};
+
+module.exports = { deleteGearItem, addGearItem }
