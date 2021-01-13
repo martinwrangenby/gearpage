517d1e886472a757aec33d445784545185952ec1
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-13 13:56:54 +0100

    Add eventlistener och functionality in Modal for catching and handling esc keypress to close Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,17 +4,34 @@ import React from 'react';
 import Backdrop from '../Backdrop/Backdrop'
 import './Modal.css';
 
-const modal = (props) => (
-  <React.Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div 
-      className="Modal" 
-      style={{
-        transform: props.show ? 'scale(1)' : 'scale(0)',
-        opacity: props.show ? '1' : '0'}}>
-      {props.show ? props.children : null}
-    </div>
-  </React.Fragment>
-);
+const Modal = (props) => {
+  const escFunction = React.useCallback((event) => {
+    if(event.keyCode === 27) {
+      props.modalClosed();
+    }
+  }, [props]);
 
-export default modal;
+  React.useEffect(() => {
+    if (props.show) {
+      document.addEventListener("keydown", escFunction, false);
+      return () => {
+        document.removeEventListener("keydown", escFunction, false);
+      };
+    }
+  }, [escFunction, props.show]);
+
+  return (
+    <React.Fragment>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div 
+        className="Modal" 
+        style={{
+          transform: props.show ? 'scale(1)' : 'scale(0)',
+          opacity: props.show ? '1' : '0'}}>
+        {props.show ? props.children : null}
+      </div>
+    </React.Fragment>
+    )
+};
+
+export default Modal;
