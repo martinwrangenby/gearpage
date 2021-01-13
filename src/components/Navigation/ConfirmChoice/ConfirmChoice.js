01557c251e3068d86a4664192603128d008b3c7a
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-13 20:50:22 +0100

    Add ConfirmChoice component

---
+++ src/components/Navigation/ConfirmChoice/ConfirmChoice.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '../../UI/Button/Button';
+import './ConfirmChoice.css';
+
+const ConfirmChoice = props => (
+  <React.Fragment>
+    <h2 className='ConfirmTitle'>{props.title}</h2>
+    <h4>Are you sure?</h4>
+    <Button buttonType='Danger' dataTestId='confirm' clicked={props.confirm}>
+      Yes
+    </Button>
+    <Button dataTestId='reject' clicked={props.reject}>
+      No
+    </Button>
+  </React.Fragment>
+)
+
+export default ConfirmChoice;
