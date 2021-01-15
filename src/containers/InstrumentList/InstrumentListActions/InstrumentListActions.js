b11d65cbb3df66b525bde13e6f9666ecef0c2414
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-16 00:27:53 +0100

    Add InstrumentsListActions component

---
+++ src/containers/InstrumentList/InstrumentListActions/InstrumentListActions.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Switch from '../../../components/UI/Switch/Switch';
+import IconButton from '../../../components/UI/IconButton/IconButton';
+import './InstrumentListActions.css';
+import gearTypes from '../../../assets/gearTypes';
+
+const InstrumentListActions = props => {
+  const [showFilter, setShowFilter] = React.useState(false);
+
+  React.useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
+
+  const handleClick = event => {
+    if (!document.getElementById('FilterDropdown').contains(event.target) &&
+      !document.getElementById('FilterButton').contains(event.target) &&
+      !document.getElementById('InstrumentTable').contains(event.target)) {
+      setShowFilter(false);
+    }
+  };
+  //TODO: make the dropdown a component of its own?
+  const dropdownContent = gearTypes.map(gearType => {
+    return (
+      <div key={gearType} className='DropdownContentWrapper'>
+        <Switch activated clicked={() => props.filterGear(gearType)} />
+        {gearType}
+      </div>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div className='TableActions'>
+        <IconButton
+          type='fa fa-sliders'
+          active={showFilter}
+          clicked={() => {setShowFilter(!showFilter);}}
+          id='FilterButton'/>
+        <IconButton
+          type='fa fa-plus'
+          clicked={props.addInstrument}
+          dataTestId='addNewInstrumentButton'/>
+      </div>
+      <div
+        className={`DropdownContent${showFilter ? '' : ' hidden'}`}
+        id='FilterDropdown'>
+        {dropdownContent}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default InstrumentListActions;
