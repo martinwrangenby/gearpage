4a6967eb54b2fada9ac685beb5bf21ec5f95fdf1
Martin Wrangenby <martinwrangenby@gmail.com>
2021-01-21 23:07:26 +0100

    Re-write HandleInstrumentForm component to support input validation and usage for Input component

--- src/components/HandleInstrumentForm/HandleInstrumentForm.js
+++ src/components/HandleInstrumentForm/HandleInstrumentForm.js
@@ -1,63 +1,108 @@
 import React from 'react';
 import Button from '../UI/Button/Button';
-import instrumentTypes from '../../assets/gearTypes';
+import Input from '../UI/Input/Input';
+import gearFormRules from '../../assets/gearFormRules';
 
-import './HandleInstrumentForm.css';
 
 // TODO: Add security on input form
 const HandleInstrumentForm = props => {
 
-  const handleSubmit = event => {
-    if (event.target.type.value && event.target.name.value) {
-      const data = {
-        name: event.target.name.value,
-        type: event.target.type.value,
-        description: event.target.description.value,
-      };
+  const [formContent, setFormContent] = React.useState(gearFormRules);
+  const [formValid, setFormValid] = React.useState(false);
+
+  React.useEffect(() => {
+    if (props.instrument) {
+      setFormContent(prevFormContent => {
+        const updatedFormContent = { ...prevFormContent };
+        Object.keys(props.instrument).forEach(identifier => {
+          if (identifier !== 'id') {
+            const updatedFormElement = { ...updatedFormContent[identifier] };
+            updatedFormElement.value = props.instrument[identifier];
+            updatedFormElement.valid = true;
+            updatedFormContent[identifier] = updatedFormElement;
+          }
+        });
+        return updatedFormContent;
+      });
+    }
+  }, [props.instrument]);
+
+  const checkValidity = (value, rules) => {
+    let isValid = true;
+    if (rules.mandatory) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    return isValid;
+  };
+
+  const handleInputChange = (event, identifier) => {
+    if (event.target.value !== formContent[identifier].value) {
+      const updatedForm = { ...formContent };
+      const updatedFormElement = { ...updatedForm[identifier] };
+      updatedFormElement.value = event.target.value;
+      updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.rules);
+      updatedFormElement.touched = true;
+      updatedForm[identifier] = updatedFormElement;
+      let formIsValid = true;
+      Object.keys(updatedForm).forEach((formItem) => {
+        formIsValid = updatedForm[formItem].valid && formIsValid;
+      });
+      setFormContent(updatedForm);
+      setFormValid(formIsValid);
+    }
+  };
+
+  const handleSubmit = () => {
+    let validSubmit = true;
+    const data = {};
+    Object.keys(formContent).forEach(identifier => {
+      if (formContent[identifier].mandatory && formContent[identifier].value === '') {
+        validSubmit = false;
+      } else {
+        data[identifier] = formContent[identifier].value;
+      }
+    });
+    if (validSubmit) {
       props.submitInstrument(data);
     } else {
       alert('you must specify both instrument type and name');
     }
-    event.preventDefault();
   };
 
-  const selectSection = props.instrument
-    ? <select name='type' id='type' data-test-id='formGearType' defaultValue={props.instrument.type}>
-      {instrumentTypes.map((type) => {
-        return <option key={type} value={type}>{type}</option>;
-      })}
-    </select>
-    : <select name='type' id='type' data-test-id='formGearType' defaultValue=''>
-      <option value='' disabled>Instrument type</option>
-      {instrumentTypes.map((type) => {
-        return <option key={type} value={type}>{type}</option>;
-      })}
-    </select>;
-
+  const formElementsArray = [];
+  for (const key in formContent) {
+    formElementsArray.push({
+      id: key,
+      config: formContent[key],
+    });
+  }
   return (
     <React.Fragment>
-      <form onSubmit={handleSubmit}>
-        {selectSection}
-        <input
-          name='name'
-          id='name'
-          type="text"
-          data-test-id='formGearName'
-          defaultValue={props.instrument ? props.instrument.name : ''}
-          placeholder='Instrument name'/>
-        <textarea
-          rows='4'
-          name='description'
-          id='description'
-          type="text"
-          data-test-id='formGearDescription'
-          defaultValue={props.instrument ? props.instrument.description : ''}
-          placeholder='Description (optional)'/>
-        <Button buttonType='Success' type='submit' dataTestId='submitGearFormButton'>
-          {props.instrument ? 'Update' : 'Add'}
-        </Button>
-        <Button buttonType='Danger' type='reset' clicked={props.closeModal}>Cancel</Button>
+      <form onSubmit={(event) => event.preventDefault()}>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            inValid={!formElement.config.valid}
+            touched={formElement.config.touched}
+            changed={(event) => handleInputChange(event, formElement.id)}
+            dataTestId={formElement.config.dataTestId}
+          />
+        ))}
       </form>
+      <Button
+        disabled={!formValid}
+        buttonType='Success'
+        clicked={handleSubmit}
+        dataTestId='submitGearFormButton'>
+        {props.instrument ? 'Update' : 'Add'}
+      </Button>
+      <Button
+        buttonType='Danger'
+        clicked={props.closeModal}>Cancel
+      </Button>
     </React.Fragment>
   );
 };
