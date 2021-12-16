5edfccf1a3e5add8c0ba89f5d37f8317f1d68ae1
Martin Wrangenby <martinwrangenby@gmail.com>
2021-12-16 21:22:25 +0100

    Update Toolbar to make use of the toolbar menu

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import IconButton from '../../UI/IconButton/IconButton';
+import ToolbarMenu from './ToolbarMenu/ToolbarMenu';
 import './Toolbar.css';
 
-const Toolbar = () => (
-  <header className='Toolbar'>
-    <NavLink to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
-      GEAR PAGE
-    </NavLink>
-  </header>
-);
+const Toolbar = (props) => {
+  React.useEffect(() => {
+    if (props.showMenu) {
+      document.addEventListener('mousedown', handleClick);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  });
+
+  const handleClick = event => {
+    if (!document.getElementById('menuButton').contains(event.target) &&
+      !document.getElementById('menuContent').contains(event.target)) {
+      props.toggleMenu(false);
+    }
+  };
+
+  return (
+    <header className='Toolbar'>
+      <div className='ToolbarContent'>
+        <IconButton
+          type='fa fa-bars'
+          id='menuButton'
+          active={props.showMenu}
+          clicked={() => props.toggleMenu(!props.showMenu)}/>
+        <NavLink to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
+          GEAR PAGE
+        </NavLink>
+        {props.showMenu ? <ToolbarMenu /> : ''}
+      </div>
+    </header>
+  );
+};
 
 export default Toolbar;
