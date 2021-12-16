a2b44b728627d281ba353a93cc1e54f55b94c9b0
Martin Wrangenby <martinwrangenby@gmail.com>
2021-12-16 21:23:17 +0100

    Update Layout HOC to handle state for toolbar menu toggling

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import './Layout.css';
 
-const Layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <main className='Content'>
-      {props.children}
-    </main>
-  </React.Fragment>
-);
+const Layout = (props) => {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  return (
+    <React.Fragment>
+      <Toolbar showMenu={menuOpen} toggleMenu={setMenuOpen}/>
+      <main className='Content'>
+        {props.children}
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default Layout;
