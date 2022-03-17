f716ccca269e47a76a89faad7c55c39bbcf5c37a
Martin Wrangenby <martinwrangenby@gmail.com>
2022-03-17 23:17:27 +0100

    Add playwright config

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config(({ path: '.env.local' }));
+
+const cliReporter = process.env.CI ? 'github' : 'list';
+
+const config = {
+  testDir: 'e2e/specs',
+  workers: process.env.HEADFUL ? 1 : 4,
+  use: {
+    headless: process.env.HEADFUL ? false : true,
+    browserName: 'chromium',
+    screenshot: 'only-on-failure',
+  },
+  outputDir: 'e2e/results',
+  reporter: [ [cliReporter], ['html', { open: 'never', outputFolder: 'e2e/results/html' }] ],
+};
+
+module.exports = config;
