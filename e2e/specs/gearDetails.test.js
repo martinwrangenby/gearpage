ce070d7d782249bfaf28e7a3dfbc2db72ad87815
Martin Wrangenby <martinwrangenby@gmail.com>
2022-03-17 23:19:04 +0100

    Update tests and run scripts according to new runner

--- e2e/specs/gearDetails.test.js
+++ e2e/specs/gearDetails.test.js
@@ -1,32 +1,37 @@
+const { test, expect } = require('@playwright/test');
 const { addGearItem, deleteGearItem } = require('../utils/firebaseAPI');
 const { bass, guitar } = require('../assets/testdata');
 let id;
 
-beforeEach(async () => {
-  id = await addGearItem(bass);
-  await page.goto(`${process.env.REACT_APP_FRONTEND}gearitem?id=${id}`, { waitUntil: 'networkidle' });
-});
+test.describe('Gear details page', () => {
+  test.beforeEach(async ({ page }) => {
+    id = await addGearItem(bass);
+    await page.goto(`${process.env.REACT_APP_FRONTEND}gearitem?id=${id}`, { waitUntil: 'networkidle' });
+  });
 
-test('Delete instrument', async () => {
-  await page.click('[data-test-id="deleteInstrument"]');
-  await page.click('[data-test-id="confirm"]');
-  await page.waitForLoadState('networkidle');
-  await expect(page).not.toHaveSelector(`#${id}`, { timeout: 1000 });
-});
+  test.afterEach(async () => {
+    await deleteGearItem(id);
+  });
 
-describe('', () => {
-  test('Edit instrument', async () => {
+  test('Delete instrument', async ({ page }) => {
+    await page.click('[data-test-id="deleteInstrument"]');
+    await page.click('[data-test-id="confirm"]');
+    await page.waitForLoadState('networkidle');
+
+    const deletedInstrument = await page.locator(`#${id}`).isVisible();
+    await expect(deletedInstrument).not.toBeTruthy();
+  });
+
+  test('Edit instrument', async ({ page }) => {
     await page.click('[data-test-id="editInstrument"]');
     await page.selectOption('[data-test-id="formGearType"]', guitar.type);
     await page.fill('[data-test-id="formGearName"]', guitar.name);
     await page.fill('[data-test-id="formGearDescription"]', guitar.description);
     await page.click('[data-test-id="submitGearFormButton"]');
     await page.waitForSelector(`.${guitar.type}`);
-    await expect(page).toEqualText('[data-test-id="gearDetailsName"]', guitar.name);
-    await expect(page).toEqualText('[data-test-id="gearDetailsDescription"]', guitar.description);
+
+    await expect(page.locator('[data-test-id="gearDetailsName"]')).toHaveText(guitar.name);
+    await expect(page.locator('[data-test-id="gearDetailsDescription"]')).toHaveText(guitar.description);
   });
 
-  afterEach(async () => {
-    await deleteGearItem(id);
-  });
 });
--- e2e/specs/gearList.test.js
+++ e2e/specs/gearList.test.js
@@ -1,22 +1,28 @@
+const { test, expect } = require('@playwright/test');
 const { deleteGearItem } = require('../utils/firebaseAPI');
 const { bass } = require('../assets/testdata');
 const name = new Date().toLocaleString();
 
-beforeEach(async () => {
-  await page.goto(process.env.REACT_APP_FRONTEND, { waitUntil: 'networkidle' });
-});
+test.describe('Gear list', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(process.env.REACT_APP_FRONTEND, { waitUntil: 'networkidle' });
+  });
 
-test('Add new instrument', async () => {
-  await page.click('[data-test-id="addNewInstrumentButton"]');
-  await page.selectOption('[data-test-id="formGearType"]', bass.type);
-  await page.fill('[data-test-id="formGearName"]', name);
-  await page.fill('[data-test-id="formGearDescription"]', bass.description);
-  await page.click('[data-test-id="submitGearFormButton"]');
-  await page.waitForLoadState('networkidle');
-  await expect(page).toHaveText(`[name="${name}"]`, name);
-});
+  test.afterEach(async ({ page }) => {
+    const id = await page.getAttribute(`[name="${name}"]`, 'id');
+    await deleteGearItem(id);
+  });
 
-afterEach(async () => {
-  const id = await page.getAttribute(`[name="${name}"]`, 'id');
-  await deleteGearItem(id);
+  test('Add new instrument', async ({ page }) => {
+    await page.click('[data-test-id="addNewInstrumentButton"]');
+    await page.selectOption('[data-test-id="formGearType"]', bass.type);
+    await page.fill('[data-test-id="formGearName"]', name);
+    await page.fill('[data-test-id="formGearDescription"]', bass.description);
+    await page.click('[data-test-id="submitGearFormButton"]');
+    await page.waitForLoadState('networkidle');
+
+    const newInstrument = page.locator(`[name="${name}"]`);
+    await expect(newInstrument).toBeVisible();
+    await expect(newInstrument).toContainText(name);
+  });
 });
