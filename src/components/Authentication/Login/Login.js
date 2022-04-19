068ac70c6c839cf6f3e9540a56462954ebc0e174
Martin Wrangenby <martinwrangenby@gmail.com>
2022-04-19 10:19:23 +0200

    Add data-test-id to login failure text element

--- src/components/Authentication/Login/Login.js
+++ src/components/Authentication/Login/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Modal from '../../../components/UI/Modal/Modal';
-import Input from '../../../components/UI/Input/Input';
-import Button from '../../../components/UI/Button/Button';
-import Switch from '../../../components/UI/Switch/Switch';
+import Modal from '../../UI/Modal/Modal';
+import Input from '../../UI/Input/Input';
+import Button from '../../UI/Button/Button';
+import Switch from '../../UI/Switch/Switch';
 
 import './Login.css';
 
@@ -11,7 +11,7 @@ const Login = ({ setPassword, setEmail, handleSwitchClick, handleSubmitButtonCli
   return(
     <Modal show={true}>
       <h1>Login plx</h1>
-      <p className='LoginError'>{errorMsg}</p>
+      <p className='LoginError' data-test-id='loginError'>{errorMsg}</p>
       <form onSubmit={(event) => event.preventDefault()}>
         <Input
           elementConfig={{ placeholder: 'Username (email)',type: 'email', autoComplete: 'on' }}
