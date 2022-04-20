550355f0a87035f5b0c445c1c6f4cff89e974453
Martin Wrangenby <martinwrangenby@gmail.com>
2022-04-20 07:43:01 +0200

    Add cancel logout e2e test

--- e2e/specs/logout.test.js
+++ e2e/specs/logout.test.js
@@ -12,4 +12,12 @@ test.describe('Logout', () => {
     await page.click('[data-test-id="confirm"]');
     await expect(page.locator('[data-test-id="loginUsername"]')).toBeVisible();
   });
+
+  test('User can cancel log out in popup', async ({ page }) => {
+    await page.click('[data-test-id="toolbarMenuButton"]');
+    await page.click('[data-test-id="toolbarMenuLogout"]');
+    await page.click('[data-test-id="reject"]');
+    await page.click('[data-test-id="toolbarMenuButton"]');
+    await expect(page.locator('#menuContent')).toBeVisible();
+  });
 });
