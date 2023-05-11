eab899657c6f230d03efb598aa452a231c379153
Martin Wrangenby <martinwrangenby@gmail.com>
2023-05-11 22:43:48 +0200

    Update login test

--- e2e/specs/login.test.js
+++ e2e/specs/login.test.js
@@ -1,37 +1,35 @@
 
 const { test, expect } = require('../config/fixtures');
 
+const { E2E_TEST_USERNAME, E2E_TEST_PASSWORD } = process.env;
+
 test.describe('Login', () => {
-  test('User can login', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('/');
-    await page.fill('[data-testid="loginUsername"]', process.env.E2E_TEST_USERNAME);
-    await page.fill('[data-testid="loginPassword"]', process.env.E2E_TEST_PASSWORD);
-    await page.click('[data-testid="loginSubmit"]');
-    await page.waitForLoadState('networkidle');
-    await page.click('[data-testid="toolbarMenuButton"]');
-    await expect(page.locator('#menuContent')).toBeVisible();
+  });
+
+  test('User can login', async ({ page }) => {
+    await page.getByTestId('loginUsername').fill(E2E_TEST_USERNAME);
+    await page.getByTestId('loginPassword').fill(E2E_TEST_PASSWORD);
+    await page.getByTestId('loginSubmit').click();
+    await page.getByTestId('toolbarMenuButton').click();
+
+    await expect(page.getByTestId('toolbarMenuLogout')).toBeVisible();
   });
 
   test('Login with incorrect password (with valid, existing email)', async ({ page }) => {
-    await page.goto('/');
-    await page.fill('[data-testid="loginUsername"]', process.env.E2E_TEST_USERNAME);
-    await page.fill('[data-testid="loginPassword"]', 'lol dummy pw');
-    await page.click('[data-testid="loginSubmit"]');
-    const loginFailure = page.locator('[data-testid="loginError"]');
+    await page.getByTestId('loginUsername').fill(E2E_TEST_USERNAME);
+    await page.getByTestId('loginPassword').fill('lol dummy pw');
+    await page.getByTestId('loginSubmit').click();
 
-    await expect(loginFailure).toContainText('Wrong email or password');
+    await expect(page.getByTestId('loginError')).toContainText('Wrong email or password');
   });
 
   test('Login with non-existing email', async ({ page }) => {
-    await page.goto('/');
-    await page.fill('[data-testid="loginUsername"]', 'thisemail@isnotaregistereduser.com');
-    await page.fill('[data-testid="loginPassword"]', 'lol dummy pw');
-    await page.click('[data-testid="loginSubmit"]');
-    const loginFailure = page.locator('[data-testid="loginError"]');
+    await page.getByTestId('loginUsername').fill('thisemail@isnotaregistereduser.com');
+    await page.getByTestId('loginPassword').fill('lol dummy pw');
+    await page.getByTestId('loginSubmit').click();
 
-    await expect(loginFailure).toContainText('Wrong email or password');
+    await expect(page.getByTestId('loginError')).toContainText('Wrong email or password');
   });
-
 });
-
-
