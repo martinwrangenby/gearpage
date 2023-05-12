26d6eb479e612d1c00fa74cd9b920ce22218344e
Martin Wrangenby <martinwrangenby@gmail.com>
2023-05-13 00:40:04 +0200

    playwright: Add webserver to config and destruct env vars

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,31 @@
 require('dotenv').config(({ path: '.env.local' }));
 
+const { CI, HEADFUL, REACT_APP_FRONTEND, BROWSERSTACK } = process.env;
 const cliReporter = process.env.CI ? 'github' : 'list';
 
 const config = {
   globalSetup: require.resolve(`${__dirname}/e2e/utils/globalSetup.js`),
   testDir: 'e2e/specs',
-  workers: process.env.HEADFUL ? 1 : 4,
-  forbidOnly: !!process.env.CI,
+  workers: HEADFUL ? 1 : 4,
+  forbidOnly: !!CI,
   use: {
-    headless: process.env.HEADFUL ? false : true,
+    headless: HEADFUL ? false : true,
     browserName: 'chromium',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
-    baseURL: process.env.REACT_APP_FRONTEND || 'http://localhost:3000/',
+    baseURL: REACT_APP_FRONTEND || 'http://localhost:3000/',
   },
   outputDir: 'e2e/results/output',
   reporter: [ [cliReporter], ['html', { open: 'never', outputFolder: 'e2e/results/html' }] ],
   retries: 0,
+  webServer:
+    !BROWSERSTACK
+      ? {
+        command: 'npm start',
+        url: 'http://localhost:3000',
+        reuseExistingServer: !CI,
+      }
+      : undefined,
 };
 module.exports = config;
