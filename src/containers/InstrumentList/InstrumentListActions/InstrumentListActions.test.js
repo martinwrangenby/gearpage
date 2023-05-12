f9bf48d76c0849bbb4a0d7b2cef21f29e82d7895
Martin Wrangenby <martinwrangenby@gmail.com>
2023-05-12 23:58:51 +0200

    Add InstrumentListActions component tests

---
+++ src/containers/InstrumentList/InstrumentListActions/InstrumentListActions.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import InstrumentListActions from './InstrumentListActions';
+
+describe('InstrumentListActions component', () => {
+  const activeFilter = ['guitar'];
+  const resetFilter = jest.fn();
+  const addInstrument = jest.fn();
+  const updateFilter = jest.fn();
+
+  beforeEach(() => {
+    render(
+      <InstrumentListActions
+        activeFilter={activeFilter}
+        resetFilter={resetFilter}
+        addInstrument={addInstrument}
+        updateFilter={updateFilter}
+      />
+    );
+  });
+
+  test('renders the component', () => {
+    expect(screen.getByTestId('instrumentListActions')).toBeInTheDocument();
+  });
+
+  test('clicking filter button shows filter dropdown', () => {
+    fireEvent.click(screen.getByTestId('filterButton'));
+    expect(screen.getByTestId('filterDropdown')).toBeInTheDocument();
+  });
+
+  test('clicking reset filters button calls resetFilter function', () => {
+    fireEvent.click(screen.getByText('Reset Filters'));
+    expect(resetFilter).toHaveBeenCalledTimes(1);
+  });
+
+  test('clicking add instrument button calls addInstrument function', () => {
+    fireEvent.click(screen.getByTestId('addNewInstrumentButton'));
+    expect(addInstrument).toHaveBeenCalledTimes(1);
+  });
+
+  test('clicking a filter switch calls updateFilter function', () => {
+    fireEvent.click(screen.getByTestId('filterButton'));
+    fireEvent.click(screen.getByTestId('guitar-filter'));
+    expect(updateFilter).toHaveBeenCalledTimes(1);
+    expect(updateFilter).toHaveBeenCalledWith('guitar');
+  });
+});
